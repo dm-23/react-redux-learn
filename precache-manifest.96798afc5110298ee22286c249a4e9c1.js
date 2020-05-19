c818008a6d71a8df88d637b3179768f8b6f4848e
Гусев Дмитрий <dm-23@yandex.ru>
2020-05-19 13:58:46 +0300

    Updates

---
+++ precache-manifest.96798afc5110298ee22286c249a4e9c1.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a89c2dc1fc8a1f04e0abde322d1a0b6",
+    "url": "/react-redux-learn/index.html"
+  },
+  {
+    "revision": "346a10e79d7df122c9a6",
+    "url": "/react-redux-learn/static/css/4.41d99fcb.chunk.css"
+  },
+  {
+    "revision": "454410f3f25001c30761",
+    "url": "/react-redux-learn/static/css/5.fb87fe5f.chunk.css"
+  },
+  {
+    "revision": "90d19eeae99b41ded368",
+    "url": "/react-redux-learn/static/css/6.d827843a.chunk.css"
+  },
+  {
+    "revision": "b2685a5a443207fbb28b",
+    "url": "/react-redux-learn/static/css/7.6b0942ac.chunk.css"
+  },
+  {
+    "revision": "f1f8f8e5d95374dd0c88",
+    "url": "/react-redux-learn/static/css/main.39705771.chunk.css"
+  },
+  {
+    "revision": "95c16fd1473a8d2bb740",
+    "url": "/react-redux-learn/static/js/0.8e19b56a.chunk.js"
+  },
+  {
+    "revision": "f7a8196d694723f10ed0",
+    "url": "/react-redux-learn/static/js/3.5f756eba.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/react-redux-learn/static/js/3.5f756eba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "346a10e79d7df122c9a6",
+    "url": "/react-redux-learn/static/js/4.570a0f3c.chunk.js"
+  },
+  {
+    "revision": "454410f3f25001c30761",
+    "url": "/react-redux-learn/static/js/5.7c468646.chunk.js"
+  },
+  {
+    "revision": "90d19eeae99b41ded368",
+    "url": "/react-redux-learn/static/js/6.5ea8cec6.chunk.js"
+  },
+  {
+    "revision": "b2685a5a443207fbb28b",
+    "url": "/react-redux-learn/static/js/7.2d0c02c1.chunk.js"
+  },
+  {
+    "revision": "f1f8f8e5d95374dd0c88",
+    "url": "/react-redux-learn/static/js/main.096129ae.chunk.js"
+  },
+  {
+    "revision": "3cf90ea89575e8c65eff",
+    "url": "/react-redux-learn/static/js/runtime-main.64d3b4f1.js"
+  },
+  {
+    "revision": "4c14021095fd20edc900e2e9b91d318e",
+    "url": "/react-redux-learn/static/media/loader.4c140210.gif"
+  }
+]);
